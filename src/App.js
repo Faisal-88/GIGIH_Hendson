674536d46188eaa4969748dc248c1b1b7e82e3b0
faisaltimbangan32@gmail.com <faisaltimbangan32@gmail.com>
2022-04-15 00:58:14 +0700

    Hendson Module 5 session 3

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import './App.css'
+import Search from './pages/Search'
+import Trending from './pages/trending'
+import { Provider } from 'react-redux'
+import Store from './Store'
+import Navbar from './pages/Navbar'
+function App () {
+  return (
+    <Provider store={Store}>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path='/trending' component={Trending} />
+          <Route path='/search' component={Search} />
+        </Switch>
+      </Router>
+
+    </Provider>
+  )
+}
+
+export default App
---
+++ src/components/searchbar/index.js
@@ -0,0 +1,11 @@
+import Button from '@mui/material/Button'
+const SearchBar = ({ query, onChange, onSubmit }) => {
+  return (
+    <form onSubmit={onSubmit}>
+      <input type='text' name='query' value={query} onChange={onChange} />
+      <Button size='small' type='submit' variant='contained'> Search</Button>
+    </form>
+  )
+}
+
+export default SearchBar
